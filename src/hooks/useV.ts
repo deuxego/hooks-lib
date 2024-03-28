f5ca6689efac7ceae0d42929cf4391b3fb93ba4c
deux <93449519+deuxego@users.noreply.github.com>
2024-03-28 21:55:33 +0200

    feat: useV

--- src/hooks/useV.ts
+++ src/hooks/useV.ts
@@ -2,11 +2,11 @@ import { useState } from "react";
 
 type SetValue<T> = (newValue: T | ((prevValue: T) => T)) => void;
 
-export const useV = <T,>(v: T) => {
+const useV = <T,>(v: T) => {
   const [value, setValue] = useState(v);
 
-  const updater = (...fn: ((set: SetValue<T>) => void)[]) => {
-    return [value, ...fn.map((f) => f.bind({value}, setValue as SetValue<T>))] as const;
+  const updater = (...fn: ((set: SetValue<T>, ...params: T[]) => void)[]) => {
+    return [value, ...fn.map((f) => f.bind(null, setValue as SetValue<T>))] as const;
   };
 
   return updater;
