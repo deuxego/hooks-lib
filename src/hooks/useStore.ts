27e6d259ce547b36fd3f253fc0102b36700518da
deuxego <binanceak1@gmail.com>
2024-03-29 18:57:16 +0200

    fix: fix

--- src/hooks/useStore.ts
+++ src/hooks/useStore.ts
@@ -10,7 +10,6 @@ export const useStore = <T,>(initialState: StateType<T>) => {
   const proxyState = new Proxy(state, {
     set(target, prop, newValue) {
       target[prop as keyof T] = newValue;
-      console.log(state)
       setState({...state, [prop as string]: newValue});
       return true;
     },
